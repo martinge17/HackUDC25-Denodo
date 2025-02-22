9b53641d6792a81ffead377d72f74a0a498760e0
martinge17 <53666932+martinge17@users.noreply.github.com>
2025-02-22 13:52:41 +0100

    Fixed once again the fallback port

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -4,7 +4,7 @@
 set -x
 
 # Set environment variable for Docker ports
-DOCKER_PORTS=${DENODO_PORT:-8080:8080}
+DOCKER_PORTS=${DENODO_PORT:-3000:3000}
 
 echo "-> Step 1: Building Docker image....."
 # Build latest app docker image
