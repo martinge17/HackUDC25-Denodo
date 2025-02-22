80e514668f376de0b49ea6117d5da112ce4c2e5b
martinge17 <53666932+martinge17@users.noreply.github.com>
2025-02-23 00:22:35 +0100

    deploy script

---
+++ backend/deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Enable debug
+set -x
+
+# Set environment variable for Docker ports
+DOCKER_PORTS=${BACKEND_PORT:-8888:80}
+
+echo "-> Step 1: Building Docker image....."
+# Build latest app docker image
+docker build . --file Dockerfile --tag localhost/denodo-chat-backend-latest
+
+echo "-> Step 1 Completed: Docker image built successfully!"
+
+echo "-> Step 2: Stopping and removing old docker containers..."
+
+# Stop and remove current docker container (if it does not exists, skip)
+docker stop denodo-chat-back || true && docker rm denodo-chat-back || true
+
+echo ${DENODO_URL}
+echo ${USER}
+echo ${PASS}
+
+echo "-> Step 2 Done"
+
+echo "-> Step 3: Running latest version...."
+# Run latest version (previously created image)
+docker run -d --name denodo-chat-back --env DENODO_URL=${DENODO_URL} USER=${USER} PASS=${PASS}  -p $DOCKER_PORTS localhost/denodo-chat-backend-latest
+
+echo "-> Step 3 Completed: Latest version deployed!"
